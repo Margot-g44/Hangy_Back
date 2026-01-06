6f10b724849f04eb1d96ca1d2febb1d60ce51368
Margot Gouriou <margot.gouriou@gmail.com>
2026-01-06 13:55:37 +0100

    initial commit

---
+++ src/main/java/Hangy/demo/HangyApplication.java
@@ -0,0 +1,13 @@
+package Hangy.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HangyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Hangy/demo/HangyApplicationTests.java
@@ -0,0 +1,13 @@
+package Hangy.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
