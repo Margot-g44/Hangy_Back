c61492a954b24021947f80b0b2d0a8cf9f60c785
Sparesotto Anais <anais.camille.sparesotto@gmail.com>
2026-01-15 11:56:16 +0100

    fix cors setup

--- src/test/java/Hangy/demo/HangyApplicationTests.java
+++ src/test/java/Hangy/demo/HangyApplicationTests.java
@@ -3,7 +3,11 @@ package Hangy.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration," +
+				"org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration," +
+				"org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration"
+})
 class HangyApplicationTests {
 
 	@Test
